8ee0ae5cf17bc91da748d8d99bd277a2ad510b58
Emad Adel <emadadel1920@gmail.com>
2025-04-10 02:35:28 +0200

    Update void.sh

--- void.sh
+++ void.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install required packages
-sudo xbps-install -y curl nano xrandr bluez bluez-alsa blueman vlc uget
+sudo xbps-install -y curl nano xrandr bluez bluez-alsa blueman vlc uget redshift
 
 # Enable Bluetooth services
 sudo ln -sf /etc/sv/bluetoothd /var/service/

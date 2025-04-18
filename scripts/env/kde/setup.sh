057209c04858722a969f7f7077da08d054e1b124
Emad Adel <emadadel1920@gmail.com>
2025-04-18 20:08:21 +0200

    Update setup.sh

--- scripts/env/kde/setup.sh
+++ scripts/env/kde/setup.sh
@@ -1,4 +1,4 @@
-echo -e "\033[33mInstalling KDE environment...\033[0m"
+echo -e "\033[33m[+] Installing KDE environment...\033[0m"
 
 # Define packages clearly using a multi-line array-like format
 read -r -d '' PkgList <<'EOF'
@@ -17,7 +17,7 @@ EOF
 
 sudo xbps-install -S $PkgList
 
-echo "[+] Enabling essential services..."
+echo "\033[33m[+] Enabling essential services...\033[0m"
 sudo rfkill unblock bluetooth
 sudo ln -sf /etc/sv/NetworkManager /var/service
 sudo ln -sf /etc/sv/dbus /var/service
@@ -26,4 +26,4 @@ sudo ln -sf /etc/sv/wireplumber /var/service
 udo ln -sf /etc/sv/bluetoothd /var/service
 sudo ln -sf /etc/sv/sddm /var/service
 sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
-echo -e "\033[33mDone.\033[0m"
+echo -e "\033[33m[!] Done.\033[0m"

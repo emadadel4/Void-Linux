16b677775ee71c5f82ebd7f54c88c99d52bac323
Emad Adel <emadadel1920@gmail.com>
2025-04-18 19:53:30 +0200

    Update setup.sh

--- env-configs/xfce/emadadel/setup.sh
+++ env-configs/xfce/emadadel/setup.sh
@@ -3,7 +3,7 @@
 clear
 
 # Update
-echo "[+] Update Void.."
+echo -e "\033[1;33m[+] Update Void..\033[0m"
 sudo xbps-install -Su -y
 #sudo xbps-install void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib -y
 
@@ -45,7 +45,7 @@ xtools
 EOF
 
 # Sync repos then install all packages at once
-echo "[+] Install required packages.."
+echo -e "\033[1;33m[+] Install required packages..\033[0m"
 sudo xbps-install -S $PkgList
 
 # Set up pipewire service
@@ -57,26 +57,26 @@ sudo xbps-install -S $PkgList
 #echo "done" >> ~/.xinitrc
 
 # Enable Bluetooth services
-echo "[+] Enable Bluetooth services..."
+echo -e "\033[1;33m[+] Enable Bluetooth services...\033[0m"
 #sudo rfkill unblock bluetooth
 #sudo ln -s /etc/sv/bluetoothd /var/service/
 
 # Add bash completion source line to .bashrc
-echo "[+] Add bash completion source line to .bashrc..."
+echo -e "\033[1;33m[+] Add bash completion source line to .bashrc...\033[0m"
 #echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
 #source ~/.bashrc
 
 # Installing fonts...
-echo "[+] Restore XFCE settings..."
+echo -e "\033[1;33m[+] Restore XFCE settings...\033[0m"
 #sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
 #sleep 2
 #tar -xzvf fonts.tar.gz -C .local/share
 
 # Restore XFCE settings
-echo "[+] Restore XFCE settings..."
+echo -e "\033[1;33m[+] Restore XFCE settings...\033[0m"
 #sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
 #sleep 2
 #tar -xzvf xfce4-config.tar.gz
 
-echo "[i] Do you want Gaming on Void"
+echo -e "\033[1;33m[i] Do you want Gaming on Void?\033[0m"
 sudo xbps-install -S wine wine-32bit winetricks lutris gamemode gamescope 

a78c175c843d38f1533bf85448983d3a3b8eb820
Emad Adel <emadadel1920@gmail.com>
2025-04-18 20:09:35 +0200

    Update setup.sh

--- env-configs/xfce/emadadel/setup.sh
+++ env-configs/xfce/emadadel/setup.sh
@@ -51,23 +51,23 @@ sudo xbps-install -S $PkgList
 #echo "done" >> ~/.xinitrc
 
 echo -e "\033[1;33m[+] Enable Bluetooth services...\033[0m"
-#sudo rfkill unblock bluetooth
-#sudo ln -s /etc/sv/bluetoothd /var/service/
+sudo rfkill unblock bluetooth
+sudo ln -s /etc/sv/bluetoothd /var/service/
 
 echo -e "\033[1;33m[+] Add bash completion source line to .bashrc...\033[0m"
-#echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
-#source ~/.bashrc
+echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
+source ~/.bashrc
 
 echo -e "\033[1;33m[+] Restore XFCE settings...\033[0m"
-#sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
-#sleep 2
-#tar -xzvf fonts.tar.gz -C .local/share
+sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
+sleep 2
+tar -xzvf fonts.tar.gz -C .local/share
 
 echo -e "\033[1;33m[+] Restore XFCE settings...\033[0m"
-#sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
-#sleep 2
+sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
+sleep 2
 #tar -xzvf xfce4-config.tar.gz
 
 echo -e "\033[1;33m[i] Do you want Gaming on Void?\033[0m"
 sudo xbps-install -S wine wine-32bit winetricks lutris gamemode gamescope 
-echo "Done."
+echo -e "\033[1;33m[+] Done.\033[0m"

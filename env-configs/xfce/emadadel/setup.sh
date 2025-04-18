906e41c441160743e3d46e2fa4d4772a5076cb6a
Emad Adel <emadadel1920@gmail.com>
2025-04-18 19:36:42 +0200

    Update setup.sh

--- env-configs/xfce/emadadel/setup.sh
+++ env-configs/xfce/emadadel/setup.sh
@@ -6,8 +6,32 @@ sudo xbps-install void-repo-nonfree void-repo-multilib-nonfree void-repo-multili
 sudo xbps-install -Su -y
 
 # Install required packages
+
+# Define packages clearly using a multi-line array-like format
+read -r -d '' PkgList <<'EOF'
+nano
+xrandr
+bluez
+blueman
+libspa-bluetooth
+vlc
+uget
+redshift
+redshift-gtk
+kitty
+bash-completion
+freerdp
+font-kacst
+SDL2_ttf
+unzip
+unrar
+git
+neofetch
+EOF
+
+# Sync repos then install all packages at once
 echo "[+] Install required packages.."
-sudo xbps-install -S -y nano xrandr bluez blueman libspa-bluetooth vlc uget redshift redshift-gtk kitty bash-completion freerdp font-kacst SDL2_ttf unzip unrar git neofetch
+sudo xbps-install -S $PkgList
 
 # Set up pipewire service
 #echo "/usr/bin/pipewire &" >> .xinitrc
@@ -18,28 +42,28 @@ sudo xbps-install -S -y nano xrandr bluez blueman libspa-bluetooth vlc uget reds
 #echo "done" >> ~/.xinitrc
 
 # Enable Bluetooth services
-echo "[+] Enable Bluetooth services..."
-sudo rfkill unblock bluetooth
-sudo ln -s /etc/sv/bluetoothd /var/service/
+#echo "[+] Enable Bluetooth services..."
+#sudo rfkill unblock bluetooth
+#sudo ln -s /etc/sv/bluetoothd /var/service/
 
 # Add bash completion source line to .bashrc
-echo "[+] Add bash completion source line to .bashrc..."
-echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
-source ~/.bashrc
+#echo "[+] Add bash completion source line to .bashrc..."
+#echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
+#source ~/.bashrc
 
 # Installing fonts...
-echo "[+] Restore XFCE settings..."
-sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
-sleep 2
-tar -xzvf fonts.tar.gz -C .local/share
+#echo "[+] Restore XFCE settings..."
+#sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
+#sleep 2
+#tar -xzvf fonts.tar.gz -C .local/share
 
 
 # Restore XFCE settings
-echo "[+] Restore XFCE settings..."
-sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
-sleep 2
-tar -xzvf xfce4-config.tar.gz
+#echo "[+] Restore XFCE settings..."
+#sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
+#sleep 2
+#tar -xzvf xfce4-config.tar.gz
 
-echo "[i] Do you want Gaming on Void?"
-sudo xbps-install -S mesa-dri-32bit mesa-vulkan-intel mesa-vulkan-intel-32bit vulkan-loader-32bit gnutls-32bit libvulkan-32bit libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit wine wine-32bit winetricks luttrs gamemode gamescope 
-echo "[i] Type 'n' to cancel"
+#echo "[i] Do you want Gaming on Void?"
+#sudo xbps-install -S mesa-dri-32bit mesa-vulkan-intel mesa-vulkan-intel-32bit vulkan-loader-32bit gnutls-32bit libvulkan-32bit libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit wine wine-32bit winetricks luttrs gamemode gamescope 
+#echo "[i] Type 'n' to cancel"

99a7c3c0781e01cc20f7c1834207231fff680f4c
Emad Adel <emadadel1920@gmail.com>
2025-04-18 19:48:21 +0200

    Update setup.sh

--- env-configs/xfce/emadadel/setup.sh
+++ env-configs/xfce/emadadel/setup.sh
@@ -52,23 +52,23 @@ sudo xbps-install -S $PkgList
 #echo "done" >> ~/.xinitrc
 
 # Enable Bluetooth services
-#echo "[+] Enable Bluetooth services..."
+echo "[+] Enable Bluetooth services..."
 #sudo rfkill unblock bluetooth
 #sudo ln -s /etc/sv/bluetoothd /var/service/
 
 # Add bash completion source line to .bashrc
-#echo "[+] Add bash completion source line to .bashrc..."
+echo "[+] Add bash completion source line to .bashrc..."
 #echo "source /usr/share/bash-completion/bash_completion" >> .bashrc
 #source ~/.bashrc
 
 # Installing fonts...
-#echo "[+] Restore XFCE settings..."
+echo "[+] Restore XFCE settings..."
 #sudo curl -L -o fonts.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/fonts.tar.gz
 #sleep 2
 #tar -xzvf fonts.tar.gz -C .local/share
 
 # Restore XFCE settings
-#echo "[+] Restore XFCE settings..."
+echo "[+] Restore XFCE settings..."
 #sudo curl -L -o xfce4-config.tar.gz https://github.com/emadadel4/Void-Linux/raw/refs/heads/main/env-configs/xfce/emadadel/xfce4-config.tar.gz
 #sleep 2
 #tar -xzvf xfce4-config.tar.gz

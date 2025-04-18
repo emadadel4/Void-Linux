c53c9582f8fa2fcc1d8278b89518408e200471b0
Emad Adel <emadadel1920@gmail.com>
2025-04-18 20:07:15 +0200

    Update setup.sh

--- scripts/env/kde/setup.sh
+++ scripts/env/kde/setup.sh
@@ -18,13 +18,12 @@ EOF
 sudo xbps-install -S $PkgList
 
 echo "[+] Enabling essential services..."
-#sudo rfkill unblock bluetooth
-#sudo ln -sf /etc/sv/NetworkManager /var/service
-#sudo ln -sf /etc/sv/dbus /var/service
-#sudo ln -sf /etc/sv/pipewire /var/service
-#sudo ln -sf /etc/sv/wireplumber /var/service
-#udo ln -sf /etc/sv/bluetoothd /var/service
-#sudo ln -sf /etc/sv/sddm /var/service
-#sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
-#echo "rebooting..."
-#sudo reboot
+sudo rfkill unblock bluetooth
+sudo ln -sf /etc/sv/NetworkManager /var/service
+sudo ln -sf /etc/sv/dbus /var/service
+sudo ln -sf /etc/sv/pipewire /var/service
+sudo ln -sf /etc/sv/wireplumber /var/service
+udo ln -sf /etc/sv/bluetoothd /var/service
+sudo ln -sf /etc/sv/sddm /var/service
+sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart/pipewire-pulse.desktop
+echo -e "\033[33mDone.\033[0m"
